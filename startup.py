addfc1e766f478b63a0e64eb2833313eeb93261a
milanmardia <72716536+milanmardia@users.noreply.github.com>
2020-12-23 21:53:18 -0600

    initialized deck_of_cards
    created a deck of cards

---
+++ startup.py
@@ -0,0 +1,16 @@
+def create_deck():
+    num_words = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King']
+    
+    suits = ["Hearts", "Spades", "Clubs", "Diamonds"]
+
+    deck_of_cards = []
+
+    for suit in suits:
+        for num in num_words:
+            deck_of_cards.append(num + " of " + suit)
+    
+    return deck_of_cards
+        
+
+
+print(create_deck())
